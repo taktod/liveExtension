1daceab2238ff8d6edfa9d538f7f92bfda1a8057
toda <toda@fc2.co.jp>
2011-03-06 08:28:26 +0900

    changed yesterday.

--- js/main.js
+++ js/main.js
@@ -3,26 +3,40 @@
  */
 var so = new SWFObject("../swf/livePlayer.swf", "live", "600", "400", "10.0", "#FFFFFF");
 var mode = "server";
+var c18 = chrome.i18n;
 $(function() {
+	language();
 	$("#server").click(function() {
 		change("server");
 		server();
-	});
-	$("#profile").click(function() {
-		change("profile");
-		profile();
 	});
 	$("#lobby").click(function() {
 		change("lobby");
 		lobby();
 	});
+	$("#myroom").click(function() {
+		change("myroom");
+		myroom();
+	});
+	$("#profile").click(function() {
+		change("profile");
+		profile();
+	});
 	change("server");
 //	server();
 });
+function language() {
+	$("#head-top").text(c18.getMessage("menu_header"));
+	$("#server").text(c18.getMessage("menu_servers"));
+	$("#profile").text(c18.getMessage("menu_profile"));
+	$("#myroom").text(c18.getMessage("menu_myroom"));
+	$("#lobby").text(c18.getMessage("menu_lobby"));
+}
 function change(page) {
 	mode = page;
 	$("#server").removeClass("now");
 	$("#profile").removeClass("now");
+	$("#myroom").removeClass("now");
 	$("#lobby").removeClass("now");
 	$("#" + page).addClass("now");
 }
@@ -30,22 +44,38 @@ function server() {
 	$("#body").html(
 		"<dl>" +
 			"<dt>" +
-				"Select your server..." +
+				c18.getMessage("servers_selected") +
+			"</dt>" +
+			"<dd>" +
+				"<input type='text' value='6a58c0282-2457b4389592' readonly='readonly'/>" +
+			"</dd>" +
+			"<dt>" +
+				c18.getMessage("servers_list") +
 			"</dt>" +
 			"<dd>" +
 				"<form>" +
-				"<select size='2'>" +
-					"<option>5eac5a6934f97ff6a58c0282-2457b4389592</option>" +
-					"<option>rtmp://163.224.148.53/ttProject/</option>" +
-				"</select>" +
-				"<br />" +
+					"<select size='2'>" +
+						"<option>6a58c0282-2457b4389592</option>" +
+						"<option>rtmp://163.224.148.53/ttProject/</option>" +
+					"</select>" +
+					"<br />" +
 					"<input type='button' value='connect'/>" +
+				"</form>" +
+			"</dd>" +
+			"<dt>" +
+				c18.getMessage("servers_add") +
+			"</dt>" +
+			"<dd>" +
+				"<form>" +
+					"<input type='text' />" +
+					"<br />" +
+					"<input type='button' value='add'/>" +
 				"</form>" +
 			"</dd>" +
 		"</dl>"
 	);
 }
-function profile() {
+function lobby() {
 	$("#body").html(
 			"<dl>" +
 			"<dt>" +
@@ -56,11 +86,22 @@ function profile() {
 		"</dl>"
 	);	
 }
-function lobby() {
+function myroom() {
 	$("#body").html(
 			"<dl>" +
 			"<dt>" +
-				"あいうえお" +
+				"hello" +
+			"</dt>" +
+			"<dd>" +
+			"</dd>" +
+		"</dl>"
+	);	
+}
+function profile() {
+	$("#body").html(
+			"<dl>" +
+			"<dt>" +
+				"hogehoge" +
 			"</dt>" +
 			"<dd>" +
 			"</dd>" +
